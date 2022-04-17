646503d6008692c7966728079f2e140eeda1f339
khanh21062002 <haduykhanh2106@gmail.com>
2022-04-17 12:19:49 +0700

    Create swich_case.java

---
+++ HelloWorld/src/swich_case.java
@@ -0,0 +1,15 @@
+public class swich_case {
+    public static void main(String []args){
+        String gender = "Boy";
+        switch(gender){
+            case "Boy":
+                System.out.println("you are boy");
+                break;
+            case "Girl":
+                System.out.println ("you are girl");
+                break;
+            default:
+                System.out.println ("We don't know");
+        }
+     }
+}
